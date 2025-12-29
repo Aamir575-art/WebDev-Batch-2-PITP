8d30c0c16f7edfb8ab88990d3b8f13a17985dffc
aamir <aamirdeveloperaamir@gmail.com>
2025-12-29 17:28:45 +0500

    week6

---
+++ week6/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {res.send('Hello, this is my express server.');});
+app.listen(port,() => { console.log(`Server is running at http://localhost:${port}`);});
+ 
